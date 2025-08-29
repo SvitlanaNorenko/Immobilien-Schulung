dde5681872e3d5b90bf927cc7bbfd72eeec7609b
sveta norenko <svetanorenko719@gmail.com>
2025-08-29 16:17:11 +0200

    comment

--- backend/src/script.js
+++ backend/src/script.js
@@ -1,15 +1,17 @@
 require("dotenv").config(); //include dotenv library to use var - env and read this file
 const questions = require("./questions.json");
 
-const { createClient } = require("@supabase/supabase-js");
+const { createClient } = require("@supabase/supabase-js"); //import the createClient function from supabase-js library
 
 const supabaseUrl = "https://uddwvhkgbecwaerhcbil.supabase.co";
 const supabase = createClient(
+  // create a new supabase client with the URL and service role key from .env file
   supabaseUrl,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 Object.keys(questions).forEach(async (topicId) => {
+  //to iterate over the keys of the questions object
   const topicQuestions = questions[topicId];
   const questionsToInsert = topicQuestions.map(
     ({ text, hasOptions, answer, options }) => ({
